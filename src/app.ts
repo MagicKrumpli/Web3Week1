83a2dcdae9b137667737f042c197ef22eb74baf3
MagicKrumpli <S00274238@atu.ie>
2026-09-16 10:35:48 +0100

    Done :D

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express, {Application, Request, Response} from "express" ;
+
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+app.get("/ping", async (_req : Request, res: Response) => {
+    res.json({
+    message: "hello from Serhii (Borya dai deneg)"
+    });
+});
+
+app.get('/bananas', async (_req : Request, res: Response) => {
+    res.json({
+    message: "this is bananas",
+    });
+});
+
+app.get('/Borya', async (_req : Request, res: Response) => {
+    res.json({
+    message: "Dai deneg bazhezhda",
+    });
+});
+
+app.use((req, _res, next) => {  
+    console.log(`${req.method} ${req.originalUrl} papapopope`);
+    next();
+});
+
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+    });
+
